bc2fddf08d6751fb361c90d48d6e6b48a73dd68a
felicityzhang <xz1574@nyu.edu>
2019-03-08 12:41:49 -0500

    finished! counld code further with walkFree and deliDishes

--- script.js
+++ script.js
@@ -2,18 +2,20 @@ const chef = document.querySelector( '#chef' )
 const upPose = document.querySelector( '.upPose' )
 const downPose = document.querySelector( '.downPose' )
 const leftPose = document.querySelector( '.leftPose' )
+const allDishs = document.querySelectorAll( '.dish' )
+const deliDish = document.querySelector( '#deliContainer' )
+const deliImage = document.querySelector( '#deliImage' )
+const changDeliDish = document.querySelector( '.changedDish' )
+const getOrder = document.querySelector( '.orderContainer' )
+
+const bell = document.querySelector( '.bell' )
+let counter = allDishs.length
 
 const RIGHT = 39
 const LEFT = 37
 const DOWN = 40
 const UP = 38
 
-
-
-// Sushi on Bar
-
-// Sushi Chef is holding
-let sushiChefHolds = ""
 let countHe = 400;
 let countVe = 400;
 document.addEventListener( 'keydown', function ( e ) {
@@ -23,7 +25,6 @@ document.addEventListener( 'keydown', function ( e ) {
         chef.className = "rightPose";
         countHe += 50;
         chef.style.left = countHe + 'px';
-
     }
 
     if ( key == UP && countVe > 250 ) {
@@ -34,68 +35,128 @@ document.addEventListener( 'keydown', function ( e ) {
 
     if ( key == DOWN && countVe < 550 ) {
         countVe += 50;
-        chef.className = "downPose";
+        chef.className = "downDishPose";
         chef.style.top = countVe + 'px';
     }
 
     if ( key == LEFT && countHe > 400 ) {
         countHe -= 50;
-        chef.className = "leftPose";
+        chef.className = "leftDishPose";
         chef.style.left = countHe + 'px';
     }
-
-    console.log( "Horizontal: " + countHe )
-    console.log( " Vertical: " + countVe )
 } )
 
+
 const removeDish = function () {
-    let allDishs = document.querySelectorAll( ".dish" );
     for ( let i = 0; i < allDishs.length; i++ ) {
         dish = allDishs[ i ]
         dish.addEventListener( 'click', function ( event ) {
             if ( countVe != 250 ) {
                 return
-                //you can not click if your vertical position is not 250
             }
-            // get difference between chef and the dish he wants to pick
-            // if the difference in an aronge 50, he can click
             else {
                 const diffValue = Math.abs( event.screenX - countHe )
-                if ( diffValue < 50 ) {
-                    console.log( allDishs[ i ].children[ 0 ].src )
-                    sushiChefHolds = allDishs[ i ].children[ 0 ].src
-                    console.log( sushiChefHolds )
+                if ( diffValue < 60 ) {
                     allDishs[ i ].remove()
-                    // console.log( event.screenX )
-                    // console.log( countHe )
+                    // allDishs[ i ].style.display = "none"
+                    counter = counter - 1
+
                 }
-
             }
-
-
-
-        } )
+        }
+        )
     }
-
 }
+
+deliDish.addEventListener( 'click', function ( event ) {
+    if ( countHe != 400 ) {
+        return
+    }
+    else {
+        const difference = Math.abs( event.screenY - countVe )
+        console.log( 'difference: ', difference )
+        if ( difference < 150 )
+            deliImage.style.display = 'block'
+        chef.className = "lookPose";
+        setTimeout( function () {
+            deliImage.style.display = 'none'
+        }, 6000 )
+
+    }
+} )
 
 function getRandomInt( min, max ) {
     min = Math.ceil( min );
     max = Math.floor( max );
-
-    return Math.floor( Math.random() * ( max - min ) ) + min; //The maximum is exclusive and the minimum is inclusive
-
+    return Math.floor( Math.random() * ( max - min ) ) + min;
 }
 
 const getDish = function () {
-    let allDishs = document.querySelectorAll( ".dish" );
-    let randomIndex = getRandomInt( 0, allDishs.length )
+    const Dishs = document.querySelectorAll( '.dish' )
+    let n = getRandomInt( 0, Dishs.length )
+    console.log( getOrder.childNodes[ 1 ].src )
+    getOrder.childNodes[ 1 ].src = Dishs[ n ].childNodes[ 1 ].src
 
+    // deliDish.addEventListener( 'click', function ( event ) {
+    //     if ( countHe != 400 ) {
+    //         return
+    //     }
+    //     else {
+    //         const difference = Math.abs( event.screenY - countVe )
+    //         if ( difference < 50 )
+    //             deliDish.src = allDishs[ n ].childNodes[ 1 ].src
+
+    //     }
+    // } )
 }
+setInterval( getDish, 5000 )
+
+
+
 const main = function () {
     removeDish()
-    // Every 15 seconds call this
     getDish()
 }
-
 main()
+
+const checkForWinner = function () {
+    chef.className = "downPose";
+    console.log( counter )
+    if ( counter === 0 ) {
+        alert( "YOU WIN!" );
+    }
+
+}
+
+bell.addEventListener( 'click', checkForWinner )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const dish1 = document.querySelector( 'dish1' )
+// const dish2 = document.querySelector( 'dish2' )
+// const dish3 = document.querySelector( 'dish3' )
+// const dish4 = document.querySelector( 'dish4' )
+// const dish5 = document.querySelector( 'dish5' )
+// const dish6 = document.querySelector( 'dish6' )
+// const dish7 = document.querySelector( 'dish7' )
+// let allDishs = [ dish1, dish2, dish3, dish4, dish5, dish6, dish7 ]
